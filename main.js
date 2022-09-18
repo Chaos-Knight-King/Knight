ff24d070431c7874dc9f88e6579f02e71524b6eb
Chaos-Knight-King <113151505+Chaos-Knight-King@users.noreply.github.com>
2022-09-18 09:46:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses)
+}
+function modelLoaded()
+{
+    console.log("PoseNet is intialized")
+}
+gotPoses()
+{
+    
+}
